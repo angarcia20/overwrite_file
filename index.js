baa986a5bb25df628a123e3d9739914fbe90e9ee
angarcia20 <angarcia20@poligran.edu.co>
2021-04-12 17:57:15 -0500

    update

--- index.js
+++ index.js
@@ -3,16 +3,64 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('repo-token');
-  console.log(`repository ${nameToGreet}!`);
+  const repoToken = core.getInput('repo-token');
+  console.log(`repository ${repoToken}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  const content = core.getInput('content');
+  console.log('Content', content);
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
+  const info =await octokit.request('GET /users/{username}', {
+    username: 'username'
+  })
 
-  const payload = github.context.payload.repository;
-  console.log('the payload : ',payload );
+  console.log(info);
+
+
+
+
+
+
+
+
+
+
+
+
+  // const {payload: {pull_request:pullRequest ,repository} } = github.context
+
+  // const repoFullName = repository?.full_name;
+
+  // if(!repoFullName || !pullRequest){
+  //   core.error('this action do not work')
+  //   // core.setOutput('comment-created','false')
+  // }else{
+  //   const {number: issueNumber} = pullRequest
+  //   const [owner,repo] = repoFullName.split("/")
+
+  //   const octokit = github.getOctokit(repoToken)
+  //   const contentFile = core.getInput('content');
+
+  //   await octokit.repos.createOrUpdateFileContents({
+  //     owner,
+  //     repo,
+  //     path: './master.xml',
+  //     message: 'update master.xml',
+  //     content: contentFile,
+  //     committer?: {
+  //       name: await octokit.request('GET /users/{username}', {
+  //         username: 'username'
+  //       })
+  //     }
+
+  //   })
+    
+
+
+
+  // }
 
 
 } catch (error) {
