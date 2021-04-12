e6b87e3bc44b9356072d1f76acfe8bd285a75d3a
angarcia20 <angarcia20@poligran.edu.co>
2021-04-12 18:42:59 -0500

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ try {
   // console.log(`The event payload: ${payload}`);
 
   const octokit = github.getOctokit(repoToken);
-   const result = await octokit.request('POST /user/emails')
+   const result = await octokit.request('GET /users')
    console.log("RESULT", result);
 
 
