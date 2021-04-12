0e736e56b24a986636e163b744234560356d01e8
angarcia20 <angarcia20@poligran.edu.co>
2021-04-12 16:26:16 -0500

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
 
-  const payload = github.context
+  const payload = github.context.repo
   console.log('the payload : ',payload );
 
 
