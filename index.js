77efe3206c708c23c5cac4a2d3b9500aeb0ac86a
angarcia20 <angarcia20@poligran.edu.co>
2021-04-12 18:09:35 -0500

    update

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
-  const octokit = github.getOctokit();
-  console.log("OCTOKIT PRINT", octokit.request('GET /users/{username}', {
-    username: 'username'
-  }))
+    await octokit.request('GET /users/{username}/hovercard', {
+      username: 'username'
+    })
 
 
 
